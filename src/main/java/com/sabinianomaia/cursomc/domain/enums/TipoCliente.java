2e9ed4fcbbbe422df3080da2e3d1f8e031f11976
“Sabiniano” <sabiniano.maia@gmail.com>
2022-01-15 11:13:39 -0300

    Cliente, TipoCliente, telefones e enderecos

---
+++ src/main/java/com/sabinianomaia/cursomc/domain/enums/TipoCliente.java
@@ -0,0 +1,39 @@
+package com.sabinianomaia.cursomc.domain.enums;
+
+public enum TipoCliente {
+	
+	PESSOAFISICA(1, "Pessoa Física"),
+	PESSOAJURIDICA(2, "Pessoa Jurídica");
+	
+	private int cod;
+	private String descricao;
+	
+	private TipoCliente(int cod, String descrcao) {
+		this.cod = cod;
+		this.descricao = descrcao;		
+	}
+	
+	public int getCod() {
+		return cod;
+	}
+	
+	public String getdescricao() {
+		return descricao;
+	}
+	
+	public static TipoCliente toEnum(Integer cod) {
+		
+		if (cod == null) {
+			return null;
+		}
+		
+		for (TipoCliente x : TipoCliente.values()) {
+			if (cod.equals(x.getCod())) {
+				return x;
+			}
+		}
+		
+		throw new IllegalArgumentException("Id inválido: " + cod);
+	}
+
+}
