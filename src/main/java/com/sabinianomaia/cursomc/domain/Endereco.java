333780d9b33ebb62312cf4153aa5d538ea9f2b4e
“Sabiniano” <sabiniano.maia@gmail.com>
2022-02-07 09:25:09 -0300

    Atualização: utilizando somente JsonIgnore

--- src/main/java/com/sabinianomaia/cursomc/domain/Endereco.java
+++ src/main/java/com/sabinianomaia/cursomc/domain/Endereco.java
@@ -10,7 +10,9 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 
-import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+//import com.fasterxml.jackson.annotation.JsonBackReference;
 
 @Entity
 public class Endereco implements Serializable {
@@ -25,7 +27,8 @@ public class Endereco implements Serializable {
 	private String bairro;
 	private String cep;
 	
-	@JsonBackReference
+	//@JsonBackReference
+	@JsonIgnore
 	@ManyToOne
 	@JoinColumn(name="client_id")
 	private Cliente cliente;
